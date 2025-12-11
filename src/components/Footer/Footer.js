442a696240739d785975f21a65182a86326813ae
aimanbaga <71024836+aimanbaga@users.noreply.github.com>
2025-12-11 07:08:15 +0300

    Remove GitHub link from Footer component
    Removed GitHub link from the footer component.

--- src/components/Footer/Footer.js
+++ src/components/Footer/Footer.js
@@ -2,12 +2,6 @@ import './Footer.css'
 
 const Footer = () => (
   <footer className='footer'>
-    <a
-      href='https://github.com/aimanbaga/'
-      className='link footer__link'
-    >
-      GitHub
-    </a>
   </footer>
 )
 
